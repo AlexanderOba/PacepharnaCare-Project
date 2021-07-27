e4f734416237254cfc681cfab72afbd817dea389
Oba Alexander <alexzy4ak@gmail.com>
2021-07-27 07:21:27 -0700

    Add files via upload

---
+++ build/precache-manifest.1dca076de9bbd23297497f226324ccde.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b2460cae339a68f9945",
+    "url": "/static/css/main.b5aaf594.chunk.css"
+  },
+  {
+    "revision": "7b2460cae339a68f9945",
+    "url": "/static/js/main.7b2460ca.chunk.js"
+  },
+  {
+    "revision": "a42d81233517f61de64a",
+    "url": "/static/css/1.cb018e54.chunk.css"
+  },
+  {
+    "revision": "a42d81233517f61de64a",
+    "url": "/static/js/1.a42d8123.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18375656992390e8e61f45edc3df10ce",
+    "url": "/static/media/section2img.18375656.png"
+  },
+  {
+    "revision": "b7fe601befa611db99dbe8ad69ebcda8",
+    "url": "/static/media/medicineplate.b7fe601b.png"
+  },
+  {
+    "revision": "c6fafdfab9206726d67d63f671b42ac2",
+    "url": "/static/media/alex.c6fafdfa.png"
+  },
+  {
+    "revision": "0efecb8335752e214dcc49f3dc602e0d",
+    "url": "/static/media/ahara.0efecb83.png"
+  },
+  {
+    "revision": "696dd61058e94fae4116f5f7c1095a95",
+    "url": "/static/media/chris.696dd610.png"
+  },
+  {
+    "revision": "04af0715c7f3b971ba00f873b864c5dd",
+    "url": "/static/media/kingsley.04af0715.png"
+  },
+  {
+    "revision": "496e45a53afb5381ea928007aad9f55b",
+    "url": "/static/media/mercy.496e45a5.png"
+  },
+  {
+    "revision": "49d64b7eb3fb593c319db5db7da91d5d",
+    "url": "/static/media/tolu.49d64b7e.png"
+  },
+  {
+    "revision": "1a61857299135daf9fedc6588f87e68e",
+    "url": "/static/media/emeka.1a618572.png"
+  },
+  {
+    "revision": "1d753fd80457a747f6f834c66f5d32e5",
+    "url": "/static/media/oye.1d753fd8.png"
+  },
+  {
+    "revision": "99b59140fff1f89e0db0844651767507",
+    "url": "/static/media/perela.99b59140.png"
+  },
+  {
+    "revision": "e3b69187791d6f7d3954dffd0408cf71",
+    "url": "/static/media/igwe.e3b69187.png"
+  },
+  {
+    "revision": "7d6e76b1f06411ef0b476c2a87c62ec3",
+    "url": "/static/media/lawal.7d6e76b1.png"
+  },
+  {
+    "revision": "14321947492accdce37c7795509a66e4",
+    "url": "/static/media/arrowhead.14321947.png"
+  },
+  {
+    "revision": "6be93fa873d206ff74311a8b4cc7c727",
+    "url": "/static/media/branding.6be93fa8.png"
+  },
+  {
+    "revision": "f6535cdb23bc938aaed01eb7210d6742",
+    "url": "/static/media/video.f6535cdb.png"
+  },
+  {
+    "revision": "22547b569055c055d1e2f7c6ef887aad",
+    "url": "/static/media/asignline.22547b56.png"
+  },
+  {
+    "revision": "f25c002f3282c6c56b3d33b071617ac2",
+    "url": "/static/media/wallpapersty.f25c002f.png"
+  },
+  {
+    "revision": "9b115054f6f40733cdc43cbe71b1b677",
+    "url": "/static/media/coils.9b115054.png"
+  },
+  {
+    "revision": "bc5e5c905f9ba22bb5bf1b1fb2f61cb2",
+    "url": "/static/media/pfizer-img.bc5e5c90.png"
+  },
+  {
+    "revision": "01fce95659ba83e2ea1a860892c1eac3",
+    "url": "/static/media/Pharmaceutical.01fce956.png"
+  },
+  {
+    "revision": "d0db22d47fd32109cc521a4d4fafdd7b",
+    "url": "/index.html"
+  }
+];
